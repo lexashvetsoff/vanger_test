a1bc6316f9c1795ea28a113dc6deb7802a8f8234
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2022-08-26 22:13:43 +0800

    add load image for slider in admin

--- promo_site/views.py
+++ promo_site/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib import admin
+
+from promo_site.models import Slider, SliderImage
 
 
 def index(request):
     print(reverse('show_promo'))
     context = {
-        'url' : reverse('show_promo')
+        'url': reverse('show_promo'),
+        'url_admin': '/admin',
     }
     return render(request, 'index.html', context)
 
 
 def show_promo(request):
-    context = {}
+    slider = Slider.objects.all()[0]
+    slider_imgs = SliderImage.objects.all().filter(slider=slider)
+    
+    imgs = [img.image.url for img in slider_imgs]
+
+    context = {
+        'imgs': imgs
+    }
     return render(request, 'promo.html', context)
